a6d0717c9e1c950274156120731d55c1304e338f
SilleryCa <139071577+SilleryCa@users.noreply.github.com>
2026-06-28 18:35:16 +0100

    Complete foundations video

---
+++ Scenes/Ocean/Ocean.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+@onready var plane: Sprite2D = $Plane
+@onready var helicopter: Sprite2D = $Helicopter
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	plane.move_local_x(100.0 * delta)
+	helicopter.move_local_x(50.0 * delta)
+	helicopter.rotate(-1 * delta)
+	
+	if Input.is_action_pressed("ui_left"):
+		plane.rotate(-1.5 * delta)
+	
+	if Input.is_action_pressed("ui_right"):
+		plane.rotate(1.5 * delta)
