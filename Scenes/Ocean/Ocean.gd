86971e42cf54ed1d7b39aace74713c3cd91bdd80
SilleryCa <139071577+SilleryCa@users.noreply.github.com>
2026-06-28 19:10:49 +0100

    add eat sound

--- Scenes/Ocean/Ocean.gd
+++ Scenes/Ocean/Ocean.gd
@@ -3,6 +3,7 @@ extends Node2D
 
 @onready var plane: Sprite2D = $Plane
 @onready var helicopter: Sprite2D = $Helicopter
+@onready var eating_sound: AudioStreamPlayer = $EatingSound
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,12 +13,15 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	plane.move_local_x(100.0 * delta)
-	helicopter.move_local_x(50.0 * delta)
-	helicopter.rotate(-1 * delta)
+	#plane.move_local_x(100.0 * delta)
+	#helicopter.move_local_x(50.0 * delta)
+	#helicopter.rotate(-1 * delta)
 	
 	if Input.is_action_pressed("ui_left"):
 		plane.rotate(-1.5 * delta)
 	
 	if Input.is_action_pressed("ui_right"):
 		plane.rotate(1.5 * delta)
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		eating_sound.play()
